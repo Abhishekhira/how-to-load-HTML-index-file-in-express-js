8e096ce2b7e8f0291c8f82e058cf52f7d021fad7
Abhishek hira <92227765+Abhishekhira@users.noreply.github.com>
2022-08-25 13:07:46 +0530

    Add files via upload

--- second.js
+++ second.js
@@ -3,6 +3,15 @@ const express=require('express')
 const app = express()
 
 const path = require('path')
+const { response } = require('express')
 const publicPath = path.join(__dirname,'public')
 app.use(express.static(publicPath))
-app.listen(4200)
+app.listen(5000)
+
+//the best way of doing this 
+app.get('',(_,resp)=>{
+    resp.sendFile(`${publicPath}/index.html`)
+})
+app.get('/about',(_,resp)=>{
+    resp.sendFile(`${publicPath}/about.html`)
+})
